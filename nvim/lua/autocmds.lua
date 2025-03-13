c644122654489f0cfd09385651fc3cb1b81d312c
thliang <lngtnhng97@gmail.com>
2025-03-13 09:47:42 +0800

    设置nvim光标回到上次离开时的位置

--- nvim/lua/autocmds.lua
+++ nvim/lua/autocmds.lua
@@ -3,3 +3,14 @@ vim.api.nvim_create_autocmd("ExitPre", {
 	command = "set guicursor=a:ver90",
 	desc = "Set cursor back to beam when leaving Neovim.",
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+	callback = function()
+		local mark = vim.api.nvim_buf_get_mark(0, '"')
+		local lcount = vim.api.nvim_buf_line_count(0)
+		if mark[1] > 0 and mark[1] <= lcount then
+			vim.api.nvim_win_set_cursor(0, mark)
+		end
+	end,
+	desc = "Set cursor to the position where it was last left.",
+})

547a480ab63be0dbcde0374894f8c6dbdae30a06
thliang <lngtnhng97@gmail.com>
2025-04-30 10:38:46 +0800

    新增treesitter-json并优化配置

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,12 +1,12 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        event = "VeryLazy",
         build = ":TSUpdate",
-        -- cmd = {'TSUpdateSync', 'TSUpdate', 'TSInstall'},
+        event = { "BufReadPost", "BufNewFile" },
         opts = {
             ensure_installed = {
                 "bash",
+                "json",
                 "lua",
                 "markdown",
                 "markdown_inline",
@@ -15,22 +15,9 @@ return {
                 "qmljs",
                 "yaml",
             },
+            auto_install = true,
             highlight = { enable = true },
         },
-        config = function(_, opts)
-            if type(opts.ensure_installed) == "table" then
-                ---@type table<string, boolean>
-                local added = {}
-                opts.ensure_installed = vim.tbl_filter(function(lang)
-                    if added[lang] then
-                        return false
-                    end
-                    added[lang] = true
-                    return true
-                end, opts.ensure_installed)
-            end
-            require("nvim-treesitter.configs").setup(opts)
-        end,
     },
     {
         "nvim-treesitter/nvim-treesitter-context",

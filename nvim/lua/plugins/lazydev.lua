fc0a3c95c2a2828d5fce9cb3f4826443867ff650
thliang <lngtnhng97@gmail.com>
2025-06-25 21:44:59 +0800

    调整lazydev插件

--- nvim/lua/plugins/lazydev.lua
+++ nvim/lua/plugins/lazydev.lua
@@ -1,12 +1,10 @@
 return {
-    {
-        "folke/lazydev.nvim",
-        ft = "lua",
-        opts = {
-            library = {
-                "luvit-meta/library",
-            },
+    "folke/lazydev.nvim",
+    dependencies = { "Bilal2453/luvit-meta", lazy = true },
+    ft = "lua",
+    opts = {
+        library = {
+            { path = "luvit-meta/library", words = { "vim%.uv" } },
         },
     },
-    { "Bilal2453/luvit-meta", lazy = true },
 }

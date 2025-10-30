ccb20b1105acba3599d64412158e16f6f76fc123
thliang <lngtnhng97@gmail.com>
2025-10-30 11:04:30 +0800

    优化cmake配置

--- nvim/lua/plugins/conform.lua
+++ nvim/lua/plugins/conform.lua
@@ -12,6 +12,7 @@ return {
     },
     opts = {
         formatters_by_ft = {
+            cmake = { "gersemi" },
             cpp = { "clang-format" },
             json = { "jq" },
             lua = { "stylua" },

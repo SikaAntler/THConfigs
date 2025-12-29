185bdcbb7fa8e7d8e10076da3c17df35bcf2f218
thliang <lngtnhng97@gmail.com>
2025-12-29 19:38:35 +0800

    修复neocmakelsp配置

--- nvim/lsp/neocmake.lua
+++ nvim/lsp/neocmake.lua
@@ -1,5 +1,5 @@
 return {
-    cmd = { "neocmakelsp", "--stdio" },
+    cmd = { "neocmakelsp", "stdio" },
     filetypes = { "cmake" },
     root_markers = { ".git", "build", "cmake" },
     single_file_support = true,

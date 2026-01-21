ad868169957ec38e0a4f78d576cae9d3660272fa
thliang <lngtnhng97@gmail.com>
2026-01-21 18:59:52 +0800

    调整colorizer插件

--- nvim/lua/plugins/colorizer.lua
+++ nvim/lua/plugins/colorizer.lua
@@ -1,5 +1,17 @@
+-- return {
+--     "brenoprata10/nvim-highlight-colors",
+--     event = "VeryLazy",
+--     opts = {},
+-- }
+
 return {
-    "brenoprata10/nvim-highlight-colors",
-    event = "VeryLazy",
-    opts = {},
+    "catgoose/nvim-colorizer.lua",
+    event = "BufReadPre",
+    opts = {
+        user_default_options = {
+            names = false,
+            RRGGBBAA = true,
+            AARRGGBB = true,
+        },
+    },
 }

2a2ceca81785af34d9c71ed10c3fdf2df741ebae
thliang <lngtnhng97@gmail.com>
2026-02-11 08:40:39 +0800

    使用codediff替换diffview

---
+++ nvim/lua/plugins/codediff.lua
@@ -0,0 +1,8 @@
+return {
+    "esmuellert/codediff.nvim",
+    lazy = true,
+    cmd = "CodeDiff",
+    keys = {
+        { "<Space>cd", "<Cmd>CodeDiff<CR>" },
+    },
+}
--- nvim/lua/plugins/diffview.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "sindrets/diffview.nvim",
-    cmd = { "DiffviewOpen", "DiffviewFileHistory" },
-    keys = {
-        { "<Space>do", "<Cmd>DiffviewOpen<CR>" },
-        { "<Space>dc", "<Cmd>DiffviewClose<CR>" },
-        { "<Space>dh", "<Cmd>DiffviewFileHistory %<CR>" },
-    },
-}
